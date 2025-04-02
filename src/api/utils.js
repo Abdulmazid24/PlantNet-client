26ba72e8c25fb1031cf61f48fdf8df364aa55006
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2025-04-02 14:44:00 +0600

    emplemented img uploaded functionality in the imgbb

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
-
-// upload image and return image URL
-export const imageUpload = async imageData => {
+// upload image and return image url
+export const imageUpload = async (imageData) => {
   const formData = new FormData();
   formData.append('image', imageData);
-  // send image data to imgbb
   const { data } = await axios.post(
-    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEy}`,
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
     formData
   );
-  const image_url = data.data.display_url;
-  return image_url;
+  return data.data.display_url;
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Home from '../pages/Home/Home'
-import ErrorPage from '../pages/ErrorPage'
-import Login from '../pages/Login/Login'
-import SignUp from '../pages/SignUp/SignUp'
-import PlantDetails from '../pages/PlantDetails/PlantDetails'
-import PrivateRoute from './PrivateRoute'
-import DashboardLayout from '../layouts/DashboardLayout'
-import AddPlant from '../pages/Dashboard/Seller/AddPlant'
-import ManageUsers from '../pages/Dashboard/Admin/ManageUsers'
-import Profile from '../pages/Dashboard/Common/Profile'
-import Statistics from '../pages/Dashboard/Common/Statistics'
-import MainLayout from '../layouts/MainLayout'
-import MyInventory from '../pages/Dashboard/Seller/MyInventory'
-import ManageOrders from '../pages/Dashboard/Seller/ManageOrders'
-import MyOrders from '../pages/Dashboard/Customer/MyOrders'
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import ErrorPage from '../pages/ErrorPage';
+import Login from '../pages/Login/Login';
+import SignUp from '../pages/SignUp/SignUp';
+import PlantDetails from '../pages/PlantDetails/PlantDetails';
+import PrivateRoute from './PrivateRoute';
+import DashboardLayout from '../layouts/DashboardLayout';
+import AddPlant from '../pages/Dashboard/Seller/AddPlant';
+import ManageUsers from '../pages/Dashboard/Admin/ManageUsers';
+import Profile from '../pages/Dashboard/Common/Profile';
+import Statistics from '../pages/Dashboard/Common/Statistics';
+import MainLayout from '../layouts/MainLayout';
+import MyInventory from '../pages/Dashboard/Seller/MyInventory';
+import ManageOrders from '../pages/Dashboard/Seller/ManageOrders';
+import MyOrders from '../pages/Dashboard/Customer/MyOrders';
 
 export const router = createBrowserRouter([
   {
@@ -95,4 +95,4 @@ export const router = createBrowserRouter([
       },
     ],
   },
-])
+]);
